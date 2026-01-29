12b6b4d1e28229b1495459816fb01661ab43a91a
ravi kumar <ark.kumar03@gmail.com>
2026-01-29 15:47:58 +0530

    rrr

--- src/components/property-steps/PropertyProfile.jsx
+++ src/components/property-steps/PropertyProfile.jsx
@@ -327,9 +327,9 @@ const PropertyProfile = ({ data = {}, onNext, updateData,isProject }) => {
     const missing = [];
   
     // 🏠 Required core fields
-    if (!bedrooms) missing.push("bedrooms");
-    if (!bathrooms) missing.push("bathrooms");
-    if (!balconies) missing.push("balconies");
+    // if (!bedrooms) missing.push("bedrooms");
+    // if (!bathrooms) missing.push("bathrooms");
+    // if (!balconies) missing.push("balconies");
     if (!areaUnit) missing.push("areaUnit");
     if (!totalFloors) missing.push("totalFloors");
     if (!status) missing.push("status");
